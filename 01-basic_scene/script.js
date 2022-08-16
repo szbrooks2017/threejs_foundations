9df271ae95d41c2533a0404ffad23616b86b640b
Stratton Brooks <69229545+szbrooks2017@users.noreply.github.com>
2022-08-16 09:07:56 -0500

    updating hierarchy

---
+++ 01-basic_scene/script.js
@@ -0,0 +1,11 @@
+// We need four elements to create a scene
+
+// Scene container
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.BoxGeometry()
+
+// Camera
+
+// Renderer
